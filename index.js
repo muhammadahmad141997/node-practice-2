7e86fc733d619cd458cfc333513ef88e6da2f99b
muhammadahmad141997 <muhammadahmad141997@gmail.com>
2024-03-17 01:36:21 +0000

    random

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.post("/insert", (req, res) => {
     let data = new user(req.body);
     let result = data.save();
     console.log(data);
-    res.send(data);
+    res.send(data)
     console.log("sucessfully inserted");
 })
 
@@ -19,19 +19,27 @@ app.post("/find",async (req, res) => {
     console.log("sucessfully Find");
 })
 
-app.delete("/delete/:_id",async (req, res) => {
+app.delete("/delete",async (req, res) => {
 
-    let data = await user.deleteOne(req.params);
+    let data = await user.deleteOne({"name":req.query.name});
     console.log(data);
     res.send(data);
     console.log("sucessfully deleted");
 })
 
-app.post("/update:data",async (req, res) => {
-    let data = await user.updateOne(req.params,{$set:req.body});
+app.post("/update/:name",async (req, res) => {
+
+    let data = await user.updateOne({"name":req.params.name},{$set:req.body});
     console.log(data);
     res.send(data);
     console.log("sucessfully updated");
 })
+
+
+
+//url param 
+//querry params =>
+//body
+//
 
 app.listen(5000);
